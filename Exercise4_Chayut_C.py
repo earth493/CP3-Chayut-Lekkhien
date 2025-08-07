10f8f5fde3af3c499d44b7b8af6d5da551994892
earth493 <rrtyuini@gmail.com>
2025-08-07 23:37:24 +0700

    exercise 4 assignment

---
+++ Exercise4_Chayut_C.py
@@ -0,0 +1,13 @@
+print("Your score generator")
+FE = input("Enter your Foundation English score: ")
+GB = input("Enter your General Business score: ")
+InCS = input("Enter your Introduction to Computer Systems score: ")
+CP = input("Enter your Computer Programming score: ")
+
+print("--- Your Score ---")
+print("Foundation English score:", FE)
+print("General Business score:", GB)
+print("Introduction to Computer Systems score:", InCS)
+print("Computer Programming score:", CP)
+
+
