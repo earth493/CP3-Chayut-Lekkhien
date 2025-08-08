16656eda2e36d1893d9ed8761d8af51fa40e2ca1
earth493 <rrtyuini@gmail.com>
2025-08-08 21:04:06 +0700

    exercise 5 assignment

---
+++ Excerise5_1_Chayut_C.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",int(num1/num2))
---
+++ Excerise5_2_Chayut_C.py
@@ -0,0 +1,4 @@
+km = int(input("Enter the distance in kilometers: "))
+hr = int(input("Enter the time in hours: "))
+velocity = km/hr
+print(int(velocity),"km/h")
